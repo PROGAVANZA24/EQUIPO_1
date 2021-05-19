872577dd2792af14f64abf5466992c9972ecae21
Alejandro Zamarron <alejandroron48@gmail.com>
2021-05-19 02:08:37 -0500

    Se creo la clase de empleado

--- Menu.py
+++
@@ -1,8 +0,0 @@
-from clase_empleado import empleado
-'''empleado1 = empleado(12, "juan", "aqui")
-empleado1.guardar()
-empleado2 = empleado(14, "richard", "diosito")
-empleado2.guardar()'''
-#empleado1.consultar_todo()
-empleado3 = empleado()
-empleado3.consultar_por_id(12)
