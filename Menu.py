4a9b876178e6de5047fb7272fbf9270827b971c5
Alejandro Zamarron <alejandroron48@gmail.com>
2021-05-19 22:56:45 -0500

    se agrego la opcion de administrar cursos al menu

--- Menu.py
+++ Menu.py
@@ -1,3 +1,4 @@
+from typing_extensions import ParamSpecKwargs
 from clase_empleado import empleado
 from clase_curso import curso
 from clase_curso_tema import curso_tema
@@ -10,8 +11,50 @@ menuprincipal = int(input("Menu principal: \n 1- Administrar empleados \n 2.- Ad
 while menuprincipal != 0:
     if menuprincipal == 1:
         menu_administrar_empleados = True
+        while menu_administrar_empleados == True:
+            opciones_menu_empleados = int(input("Menu de administracion de empleados: \n 1-Crear nuevo empleado \n 2- Consultar todos los empleados \n 3- Consultar a detalle \n"))
+            if opciones_menu_empleados == 1:
+                eid = int(input("Ingrese el ID del empleado"))
+                enom = input("ingrese el nombre ")
+                edi = input("Ingrese la direccion del empleado")
+                empleado1 = empleado(eid, enom, edi)
+                empleado1.guardar()
+                print("se ha guardado empleado")
+            elif opciones_menu_empleados == 2:
+                print("Empleados:")
+                empleado1 = empleado()
+                empleado1.consultar_todo
+            elif opciones_menu_empleados == 3:
+                id_emple = input("ingrese el id de empleado:\n")
+                empleado1 = empleado()
+                empleado1.consultar_por_id(id_emple)
+            elif opciones_menu_empleados == 0:
+                menu_administrar_empleados = False
+            elif opciones_menu_empleados >= 4:
+                menu_administrar_empleados = False
     elif menuprincipal == 2:
-        pass
+        menu_administrar_cursos = True
+        while menu_administrar_cursos == True:
+            opciones_menu_cursos = int(input("Menu de administracion de cursos: \n 1-Crear nuevo curso \n 2- Consultar todos los curso \n 3- Consultar a detalle \n"))
+            if opciones_menu_cursos == 1:
+                cid = int(input("Ingrese el id del curso: "))
+                cde = input("Ingrese una descripcion: ")
+                emid = int(input("ingrese un id al empleado: "))
+                curso1 = curso(cid, cde, emid)
+                curso1.guardar()
+                print("Se guardo curso")
+            elif opciones_menu_cursos == 2:
+                print("Cursos: ")
+                curso1 = curso()
+                curso1.consultar_todo
+            elif opciones_menu_cursos == 3:
+                id_cur = input("Ingrese id del curso que busca: ")
+                curso1 = curso()
+                curso1.consultar_por_id(id_cur)
+            elif opciones_menu_cursos == 0:
+                menu_administrar_cursos = False
+            elif opciones_menu_cursos >= 4:
+                menu_administrar_cursos = False
     elif menuprincipal == 3:
         pass
     elif menuprincipal == 4:
@@ -23,15 +66,3 @@ while menuprincipal != 0:
     else:
         print("Opcion no valida")
     menuprincipal = int(input("Menu principal: \n 1- agregar empleados \n 2.- Administrar cursos \n 3- Administrar temas \n 4- Administrar videos \n 5-Administrar los temas asignados al curso \n 6- Administrar los videos asignados a los temas del curso \n"))
-while menu_administrar_empleados == True:
-    opciones_menu_empleados = int(input("Menu de administracion de empleados: \n 1-Crear nuevo empleado \n 2- Consultar todos los empleados \n 3- Consultar a detalle \n"))
-    if opciones_menu_empleados == 1:
-        pass
-    elif opciones_menu_empleados == 2:
-        pass
-    elif opciones_menu_empleados == 3:
-        pass
-    elif opciones_menu_empleados == 0:
-        menu_administrar_empleados = False
-    elif opciones_menu_empleados >= 4:
-        menu_administrar_empleados = False
