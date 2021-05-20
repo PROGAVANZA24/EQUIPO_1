43bc551e606b8b5548eb71570220aacba4bb1050
Alejandro Zamarron <alejandroron48@gmail.com>
2021-05-19 20:51:40 -0500

    He agregado las opciones al menu

--- Menu.py
+++ Menu.py
@@ -5,3 +5,11 @@ from clase_curso_tema_video import curso_tema_video
 from clase_video import video
 from clase_tema import tema
 
+print("Menu")
+print("Elija una opcion:")
+print("1.- Administrar empleados")
+print("2.- Administrar cursos")
+print("3.- Administrar temas")
+print("4.- Administrar videos")
+print("5.- Administrar los temas asignados al curso")
+print("6.- Administrar los videos asignados a los temas del curso")
