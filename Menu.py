e3cb34e593f4957a59cf5f5e3920e01e4d81733f
Alejandro Zamarron <alejandroron48@gmail.com>
2021-05-19 21:27:39 -0500

    He puesto las opciones al menu

--- Menu.py
+++ Menu.py
@@ -6,3 +6,19 @@ from clase_video import video
 from clase_tema import tema
 
 menuprincipal = int(input("Menu principal: \n 1- Administrar empleados \n 2.- Administrar cursos \n 3- Administrar temas \n 4- Administrar videos \n 5-Administrar los temas asignados al curso \n 6- Administrar los videos asignados a los temas del curso \n"))
+while menuprincipal != 0:
+    if menuprincipal == 1:
+        pass
+    elif menuprincipal == 2:
+        pass
+    elif menuprincipal == 3:
+        pass
+    elif menuprincipal == 4:
+        pass
+    elif menuprincipal == 5:
+        pass
+    elif menuprincipal == 6:
+        pass
+    else:
+        print("Opcion no valida")
+    menuprincipal = int(input("Menu principal: \n 1- Administrar empleados \n 2.- Administrar cursos \n 3- Administrar temas \n 4- Administrar videos \n 5-Administrar los temas asignados al curso \n 6- Administrar los videos asignados a los temas del curso \n"))
