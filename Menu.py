7c76a8fe565cc8df6d0732e6bfae018b8d68a5b4
Alejandro Zamarron <alejandroron48@gmail.com>
2021-05-19 21:03:06 -0500

    He optimizado mi menu

--- Menu.py
+++ Menu.py
@@ -5,11 +5,4 @@ from clase_curso_tema_video import curso_tema_video
 from clase_video import video
 from clase_tema import tema
 
-print("Menu")
-print("Elija una opcion:")
-print("1.- Administrar empleados")
-print("2.- Administrar cursos")
-print("3.- Administrar temas")
-print("4.- Administrar videos")
-print("5.- Administrar los temas asignados al curso")
-print("6.- Administrar los videos asignados a los temas del curso")
+menuprincipal = int(input("Menu principal: \n 1- Administrar empleados \n 2.- Administrar cursos \n 3- Administrar temas \n 4- Administrar videos \n 5-Administrar los temas asignados al curso \n 6- Administrar los videos asignados a los temas del curso \n"))
