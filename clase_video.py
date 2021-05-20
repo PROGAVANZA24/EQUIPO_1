1ddea9b46d00ce163af5e75df85eed1fb16cbcd6
GersonBetancourt <78452053+GersonBetancourt@users.noreply.github.com>
2021-05-19 23:32:47 -0500

    Add files via upload

--- clase_video.py
+++ clase_video.py
@@ -1,25 +1,25 @@
-class video:
-    def __init__(self, id_video = 0, nombre = "DEFAULT", url = "DEFAULT.com", fecha_publicacion = "DEFAULT"):
-        self.id_video = id_video
-        self.nombre = nombre
-        self.url = url
-        self.fecha_publicacion = fecha_publicacion
+video de clase :
+    def  __init__ ( self , id_video  =  0 , nombre  =  "DEFAULT" , url  =  "DEFAULT.com" , fecha_publicacion  =  "DEFAULT" ):
+        yo . id_video  =  id_video
+        yo . nombre  =  nombre
+        yo . url  =  url
+        yo . fecha_publicacion  =  fecha_publicacion
         
-    def guardar(self):
-        f = open("C:\\Users\\McAik\\OneDrive\\Documentos\\FACPYA\\SEGUNDO SEMESTRE\\Programacion Avanzada\\Programa PIA\\video.txt", encoding="utf8")
-        f.write(f'id_video = {self.id_video} nombre = {self.nombre} url = {self.url} fecha_publicacion = {self.fecha_publicacion}')
-        f.close
+    def  guardar ( yo ):
+        f  =  open ( "C: \\ Users \\ GBetaGlo \\ OneDrive \\ Documentos \\ FACPYA \\ SEGUNDO SEMESTRE \\ Programacion Avanzada \\ Programa PIA \\ video.txt" , encoding = "utf8" )
+        f . write ( f'id_video = { self . id_video } nombre = { self . nombre } url = { self . url } fecha_publicacion = { self . fecha_publicacion } ' )
+        f . cerca
     
-    def consultar_todo(self):
-        f = open("C:\\Users\\McAik\\OneDrive\\Documentos\\FACPYA\\SEGUNDO SEMESTRE\\Programacion Avanzada\\Programa PIA\\video.txt")
-        print(f.read())
-        f.close
+    def  consultar_todo ( yo ):
+        f  =  open ( "C: \\ Usuarios \\ GBetaGlo \\ OneDrive \\ Documentos \\ FACPYA \\ SEGUNDO SEMESTRE \\ Programacion Avanzada \\ Programa PIA \\ video.txt" )
+        imprimir ( f . leer ())
+        f . cerca
     
-    def consultar_por_id(self, id):
-        self.id = str(id)
-        f = open("C:\\Users\\McAik\\OneDrive\\Documentos\\FACPYA\\SEGUNDO SEMESTRE\\Programacion Avanzada\\Programa PIA\\video.txt")
-        for sentencia in f:
-            recorrido = sentencia.strip().split()
-            if recorrido[2] == self.id:
-                print(recorrido)
-        f.close       
+    def  consultar_por_id ( self , id ):
+        yo . id  =  str ( id )
+        f  =  open ( "C: \\ Usuarios \\ GBetaGlo \\ OneDrive \\ Documentos \\ FACPYA \\ SEGUNDO SEMESTRE \\ Programacion Avanzada \\ Programa PIA \\ video.txt" )
+        para  sentencia  en  f :
+            recorrido  =  sentencia . tira (). dividir ()
+            if  recorrido [ 2 ] ==  self . id :
+                imprimir ( recorrido )
+        f . cerca       
