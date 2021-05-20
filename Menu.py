00fbe923e126a8b9909bc2902c90ce44c7df70d5
Alejandro Zamarron <alejandroron48@gmail.com>
2021-05-19 22:02:54 -0500

    Cree el menu para administrar empleados

--- Menu.py
+++ Menu.py
@@ -5,10 +5,11 @@ from clase_curso_tema_video import curso_tema_video
 from clase_video import video
 from clase_tema import tema
 
+
 menuprincipal = int(input("Menu principal: \n 1- Administrar empleados \n 2.- Administrar cursos \n 3- Administrar temas \n 4- Administrar videos \n 5-Administrar los temas asignados al curso \n 6- Administrar los videos asignados a los temas del curso \n"))
 while menuprincipal != 0:
     if menuprincipal == 1:
-        pass
+        menu_administrar_empleados = True
     elif menuprincipal == 2:
         pass
     elif menuprincipal == 3:
@@ -21,4 +22,16 @@ while menuprincipal != 0:
         pass
     else:
         print("Opcion no valida")
-    menuprincipal = int(input("Menu principal: \n 1- Administrar empleados \n 2.- Administrar cursos \n 3- Administrar temas \n 4- Administrar videos \n 5-Administrar los temas asignados al curso \n 6- Administrar los videos asignados a los temas del curso \n"))
+    menuprincipal = int(input("Menu principal: \n 1- agregar empleados \n 2.- Administrar cursos \n 3- Administrar temas \n 4- Administrar videos \n 5-Administrar los temas asignados al curso \n 6- Administrar los videos asignados a los temas del curso \n"))
+    while menu_administrar_empleados == True:
+        opciones_menu_empleados = int(input("Menu de administracion de empleados: \n 1-Crear nuevo empleado \n 2- Consultar todos los empleados \n 3- Consultar a detalle \n"))
+        if opciones_menu_empleados == 1:
+            print("creando")
+        elif opciones_menu_empleados == 2:
+            print("creando")
+        elif opciones_menu_empleados == 3:
+            print("creando")
+        elif opciones_menu_empleados == 0:
+            menu_administrar_empleados = False
+        elif opciones_menu_empleados >= 4:
+            menu_administrar_empleados = False
