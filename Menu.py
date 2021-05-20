372360beee4015bb6d1882b3d505aa3e26aa42ae
Alejandro Zamarron <alejandroron48@gmail.com>
2021-05-19 20:38:45 -0500

    He agregado las clases al menu

--- Menu.py
+++ Menu.py
@@ -1,2 +1,7 @@
 from clase_empleado import empleado
 from clase_curso import curso
+from clase_curso_tema import curso_tema
+from clase_curso_tema_video import curso_tema_video
+from clase_video import video
+from clase_tema import tema
+
